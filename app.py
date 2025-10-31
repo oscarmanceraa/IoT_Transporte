c8bd3b135df2d7de68f2edccdc1bd62015ea3c52
oscarmanceraa <oscarmancera20@gmail.com>
2025-10-30 22:40:17 -0500

    primer pagina powerbi

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Proyecto final bigdataaaa!"
+def PrimerPWBI():
+    return render_template("PrimerPWBI.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
